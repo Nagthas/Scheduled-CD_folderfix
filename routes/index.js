9d6ac4c0055e8a099719ad4c37e1cec5561c12d4
Nagthas <58342518+Nagthas@users.noreply.github.com>
2020-01-15 10:29:13 +0100

    Ip removal

--- routes/index.js
+++ routes/index.js
@@ -5,75 +5,75 @@ const fs = require('fs');
 var scheduledTimes = require('../public/scheduledTimes.json');
 var scheduledTimesBackup = require('../public/scheduledTimes-backup.json');
 var variables = require('../public/variables.json');
-var myipjson = require('../public/myip.json');
+// var myipjson = require('../public/myip.json');
 const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
 //console.log(variables.offsetTime);
-var myIpArray= [];
-var myLocalip = myipjson.myIp+":3000";
+// var myIpArray= [];
+// var myLocalip = myipjson.myIp+":3000";
 //--------------------------------------------------
 
 //--------------------------------------------------
 //--get all ip addresses ---- THIS ONE !!!!!!!!!!!!
 //--------------------------------------------------
-var getNetworkIPs = (function () {
-    var ignoreRE = /^(127\.0\.0\.1|::1|fe80(:1)?::1(%.*)?)$/i;
-
-    var exec = require('child_process').exec;
-    var cached;
-    var command;
-    var filterRE;
-
-    switch (process.platform) {
-    case 'win32':
-    //case 'win64': // TODO: test
-        command = 'ipconfig';
-        filterRE = /\bIPv[46][^:\r\n]+:\s*([^\s]+)/g;
-        break;
-    case 'darwin':
-        command = 'ifconfig';
-        filterRE = /\binet\s+([^\s]+)/g;
-        // filterRE = /\binet6\s+([^\s]+)/g; // IPv6
-        break;
-    default:
-        command = 'ipconfig';
-        filterRE = /\binet\b[^:]+:\s*([^\s]+)/g;
-        // filterRE = /\binet6[^:]+:\s*([^\s]+)/g; // IPv6
-        break;
-    }
-
-    return function (callback, bypassCache) {
-        if (cached && !bypassCache) {
-            callback(null, cached);
-            return;
-        }
-        // system call
-        exec(command, function (error, stdout, sterr) {
-            cached = [];
-            var ip;
-            var matches = stdout.match(filterRE) || [];
-            //if (!error) {
-            for (var i = 0; i < matches.length; i++) {
-                ip = matches[i].replace(filterRE, '$1')
-                if (!ignoreRE.test(ip)) {
-                    cached.push(ip);
-                }
-            }
-            //}
-            callback(error, cached);
-        });
-    };
-})();
-getNetworkIPs(function (error, ip) {
-myIpArray = ip
-// console.log("myIpArray: "+myIpArray);
-
-if (error) {
-    console.log('error:', error);
-}
-}, false);
-//--------------------------------------------------
-console.log("index.js -> myipjson");
-console.log(myipjson.myIp);
+// var getNetworkIPs = (function () {
+//     var ignoreRE = /^(127\.0\.0\.1|::1|fe80(:1)?::1(%.*)?)$/i;
+//
+//     var exec = require('child_process').exec;
+//     var cached;
+//     var command;
+//     var filterRE;
+//
+//     switch (process.platform) {
+//     case 'win32':
+//     //case 'win64': // TODO: test
+//         command = 'ipconfig';
+//         filterRE = /\bIPv[46][^:\r\n]+:\s*([^\s]+)/g;
+//         break;
+//     case 'darwin':
+//         command = 'ifconfig';
+//         filterRE = /\binet\s+([^\s]+)/g;
+//         // filterRE = /\binet6\s+([^\s]+)/g; // IPv6
+//         break;
+//     default:
+//         command = 'ifconfig';
+//         filterRE = /\binet\b[^:]+:\s*([^\s]+)/g;
+//         // filterRE = /\binet6[^:]+:\s*([^\s]+)/g; // IPv6
+//         break;
+//     }
+//
+//     return function (callback, bypassCache) {
+//         if (cached && !bypassCache) {
+//             callback(null, cached);
+//             return;
+//         }
+//         // system call
+//         exec(command, function (error, stdout, sterr) {
+//             cached = [];
+//             var ip;
+//             var matches = stdout.match(filterRE) || [];
+//             //if (!error) {
+//             for (var i = 0; i < matches.length; i++) {
+//                 ip = matches[i].replace(filterRE, '$1')
+//                 if (!ignoreRE.test(ip)) {
+//                     cached.push(ip);
+//                 }
+//             }
+//             //}
+//             callback(error, cached);
+//         });
+//     };
+// })();
+// getNetworkIPs(function (error, ip) {
+// myIpArray = ip
+// // console.log("myIpArray: "+myIpArray);
+//
+// if (error) {
+//     console.log('error:', error);
+// }
+// }, false);
+// //--------------------------------------------------
+// console.log("index.js -> myipjson");
+// console.log(myipjson.myIp);
 
 
 
